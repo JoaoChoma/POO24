8fb6088c0c8ba507830d015b0949ce95b5bb152d
João Choma Neto <jchomax@gmail.com>
2024-10-28 13:05:46 -0300

    SEMANA 14

---
+++ SEMANA14/crudproject/src/test/java/com/example/crudproject/CrudprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
